61258acf0c6ba0d7cbc9e68ceb8bb192a20e30ab
Roy Andy <roycabrera80@gmail.com>
2020-04-12 16:46:14 -0500

    Updates

---
+++ precache-manifest.70874d2972ce6cf3672ae93de882596e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076b043c45cc1e04970dbbb8e5641517",
+    "url": "/control-presupuesto/index.html"
+  },
+  {
+    "revision": "b3eece4de82b10ccc5c1",
+    "url": "/control-presupuesto/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "5e3ce84f5d75272b48de",
+    "url": "/control-presupuesto/static/js/2.9e49de20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/control-presupuesto/static/js/2.9e49de20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3eece4de82b10ccc5c1",
+    "url": "/control-presupuesto/static/js/main.56044723.chunk.js"
+  },
+  {
+    "revision": "58368cccbdae48dbbd42",
+    "url": "/control-presupuesto/static/js/runtime-main.e46c93ef.js"
+  }
+]);
